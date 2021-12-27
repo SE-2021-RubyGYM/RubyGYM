7d604aadfe7ce71db52f3beb1064c7fd3fb2c5fc
NguyenLoi851 <60845268+NguyenLoi851@users.noreply.github.com>
2021-12-27 22:40:11 +0700

    sua loi dang bai

--- server/models/adv.js
+++ server/models/adv.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const advSchema = new Schema({
-  _id: { type: Number },
+  // _id: { type: Number },
   creator: {
     type: String,
-    required: [true, "Thiếu thông tin người tạo quảng cáo"],
+    // required: [true, "Thiếu thông tin người tạo quảng cáo"],
   },
   title: { type: String },
-  view: { type: Number ,default:0},
+  view: { type: Number, default: 0 },
   time: { type: String },
   picture: { type: String },
-  content: { type: String, required: [true, "Thiếu nội dung quảng cáo"] },
+  content: { 
+    type: String, 
+    // required: [true, "Thiếu nội dung quảng cáo"] 
+  },
 });
 module.exports = mongoose.model("advertises", advSchema);
